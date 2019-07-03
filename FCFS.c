441e0318c16a65abd944d23a15719ea89fe44808
Ming <lumingabby@outlook.com>
2019-07-03 17:08:38 -0400

    debugged and added output function 7/3

--- FCFS.c
+++ FCFS.c
@@ -6,10 +6,7 @@
 void FCFS(struct Process* processList[], int NUM_PROCESSES, int CS_TIME){
 
     //output each process in the process list
-    for(int i=0;i<NUM_PROCESSES;i++){
-        printf("Process %d [NEW] (arrival time %d ms) %d CPU bursts\n",
-                processList[i]->Type,processList[i]->arrivalTime,processList[i]->numCPU);
-    }
+    outEachProcess(processList, NUM_PROCESSES);
 
     //initializing ready queue
     struct Queue* readyQueue = initizlizeQueue(NUM_PROCESSES);
@@ -19,10 +16,10 @@ void FCFS(struct Process* processList[], int NUM_PROCESSES, int CS_TIME){
             printf("time %dms: Simulator started for FCFS [Q <empty>]\n",time);
         }
         //if all process done with their CPU bursts, break out of the loop
-        if(allDone){
+        if(allDone(processList,NUM_PROCESSES)){
             //restore the count of done CPU
             for(int i=0;i<NUM_PROCESSES;i++){
-                processList[i]->doneCPU=processList[i]->numCPU;
+                processList[i]->doneCPU=0;
             }
             //break out of the loop and finishes FCFS
             printf("time %dms: Simulator ended for FCFS ",time);
@@ -61,19 +58,26 @@ void FCFS(struct Process* processList[], int NUM_PROCESSES, int CS_TIME){
             if(processList[i]->state==RUNNING && time==processList[i]->nextInterest){
                 //get the actual I/O burst time for the current process
                 int idx=processList[i]->doneCPU;
-                double ioTime=processList[i]->ioBurstTime[idx];
-                int leftCPU=processList[i]->numCPU-processList[i]->doneCPU;// #of CPU left undone
-                double finiIO=processList[i]->nextInterest+ioTime;// time when the process finishing IO
-                printf("time %dms: Process %d completed a CPU burst; %d bursts to go ",
-                        time,processList[i]->Type,leftCPU);
-                printf("time %dms: Process %d switching out of CPU; will block on I/O until time %lfms ",
-                        time,processList[i]->Type,finiIO);
-                //print out ready queue
-                printQueue(readyQueue);
-                //update
-                processList[i]->state=BLOCKED;
-                processList[i]->nextInterest+=ioTime;
-                processList[i]->doneCPU++;
+                //when the process is already done with all of its CPU bursts
+                if(idx+1==processList[i]->numCPU){
+                    processList[i]->state=TERMINATED;
+                    processList[i]->doneCPU++;
+                }
+                else{
+                    double ioTime=processList[i]->ioBurstTime[idx];
+                    int leftCPU=processList[i]->numCPU-processList[i]->doneCPU;// #of CPU left undone
+                    double finiIO=processList[i]->nextInterest+ioTime;// time when the process finishing IO
+                    printf("time %dms: Process %d completed a CPU burst; %d bursts to go ",
+                           time,processList[i]->Type,leftCPU);
+                    printf("time %dms: Process %d switching out of CPU; will block on I/O until time %lfms ",
+                           time,processList[i]->Type,finiIO);
+                    //print out ready queue
+                    printQueue(readyQueue);
+                    //update
+                    processList[i]->state=BLOCKED;
+                    processList[i]->nextInterest+=ioTime;
+                    processList[i]->doneCPU++;
+                }
             }
             //process is finishing I/O
             if(processList[i]->state==BLOCKED && time==processList[i]->nextInterest){
@@ -88,7 +92,7 @@ void FCFS(struct Process* processList[], int NUM_PROCESSES, int CS_TIME){
                 processList[i]->nextInterest+=CS_TIME/2;
             }
             //process is terminating
-            if(processList[i]->doneCPU==processList[i]->numCPU){
+            if(processList[i]->state==TERMINATED || processList[i]->doneCPU==processList[i]->numCPU){
                 printf("time %dms: Process %d terminated ",
                         time,processList[i]->Type);
                 //print out ready queue
